a52aa5f438aee4d18323c9cc520af8c49be917fd
germiir <pavel.latka.2@gmail.com>
2020-11-14 23:08:04 +0100

    Console logs numbers!

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ const divide = document.getElementById("divide");
 const multiply = document.getElementById("multiply");
 const point = document.getElementById("point");
 const equals = document.getElementById("equals");
+
+function buttonPress(x) {
+  switch(x) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 0:
+      console.log(x);
+      break;
+  }
+}
